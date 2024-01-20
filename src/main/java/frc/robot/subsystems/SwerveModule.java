7cd4daa64f4edce69e3d5f5e3dd8350c227f9790
IntelBamboo <xcommanderminhx@gmail.com>
2024-01-19 22:35:49 -0500

    1/19/2024

--- src/main/java/frc/robot/subsystems/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -1,11 +1,16 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.AnalogInput;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.Constants.DriveConstants;
 import frc.robot.Constants.ModuleConstants;
 
 public class SwerveModule {
@@ -46,8 +51,63 @@ public class SwerveModule {
             turningEncoder.setPositionConversionFactor(ModuleConstants.kTurningEncoderRPM2RadPerSec);
 
             turningPidController = new PIDController(ModuleConstants.kPTurning, 0, 0);
-            turningPidController.enableContinuousInput(-Math.PI, Math.PI);
+            turningPidController.enableContinuousInput(-Math.PI, Math.PI);  
+            
 
+            resetEncoders();
+        }
+    
+        public double getDrivePosition(){
+            return this.driveEncoder.getPosition();
+        }
 
+        public double getTurningPosition(){
+            return this.turningEncoder.getPosition();
+        }
+
+        public double getDriveVelocity(){
+            return this.driveEncoder.getVelocity();
+        }
+
+        public double getTurningVelocity(){
+            return this.turningEncoder.getVelocity();
+        }
+
+        public double getAbsoluteEncoderRad(){
+            double angle = absoluteEncoder.getVoltage()/ RobotController.getVoltage5V();
+            angle *= 2.0 * Math.PI; //In radians
+            angle -= absoluteEncoderOffsetRad;
+            return angle * (absoluteEncoderReversed ? -1.0 : 1.0);
+
+        }
+
+        public void resetEncoders(){
+            driveEncoder.setPosition(0);
+            turningEncoder.setPosition(getAbsoluteEncoderRad());
+        }
+
+        /**
+         * Returns in the format compatible with the WPILib Library
+         * @return
+         */
+
+        public SwerveModuleState getState(){
+            return new SwerveModuleState(getDriveVelocity(), new Rotation2d(getTurningPosition()));
+        }
+
+        public void setDesiredState(SwerveModuleState state){
+            if(Math.abs(state.speedMetersPerSecond)< 0.001){
+                stop();
+                return;
+            }
+            state = SwerveModuleState.optimize(state, getState().angle);
+            driveMotor.set(state.speedMetersPerSecond / DriveConstants.kPhysicalMaxSpeedMetersPerSecond);
+            turningMotor.set(turningPidController.calculate(getTurningPosition(), state.angle.getRadians()));
+            SmartDashboard.putString("Swerve[" + absoluteEncoder.getChannel() + "] state ", state.toString());
+        }
+
+        public void stop(){
+            driveMotor.set(0);
+            turningMotor.set(0);
         }
 }
